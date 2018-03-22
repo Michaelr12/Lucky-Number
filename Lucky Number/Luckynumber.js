49f53e02a45e15545734123b12fcedec1269cb0f
Michaelr12 <35987510+Michaelr12@users.noreply.github.com>
2018-03-21 19:21:19 -0700

    Add files via upload

---
+++ Lucky Number/Luckynumber.js
@@ -0,0 +1,60 @@
+
+
+function lotto(){
+	
+		var a = Math.floor(Math.random() * 100);
+	
+	var b = Math.floor(Math.random() * 100);
+
+	var c = Math.floor(Math.random() * 100);
+	
+	var d = Math.floor(Math.random() * 100);
+
+	var e = Math.floor(Math.random() * 100);
+
+	var f = Math.floor(Math.random() * 100);
+
+
+	if(a !== b && b !== c && c !== d && d !== e && e !== f) {
+	document.write(a + "," + b + "," + c + "," + d + "," + e + "," + f); 
+
+}
+
+// var number = document.querySelector("span");
+
+// button.addEventListener("click", function(){
+// 	reset();
+// })
+
+
+
+
+
+// 	for(var i = 0; i < 6; i++) {
+
+// 	var math(i) = Math.floor(Math.random() * 100)
+// 		if(a !== b && b !== c && c !== d && d !== e && e !== f) {
+// 	document.write(a + "," + b + "," + c + "," + d + "," + e + "," + f); 
+
+// }
+// 	}
+	
+
+
+// 	var b = Math.floor(Math.random() * 100);
+
+// 	var c = Math.floor(Math.random() * 100);
+	
+// 	var d = Math.floor(Math.random() * 100);
+
+// 	var e = Math.floor(Math.random() * 100);
+
+// 	var f = Math.floor(Math.random() * 100);
+
+// 	if(a !== b && b !== c && c !== d && d !== e && e !== f) {
+// 	document.write(a + "," + b + "," + c + "," + d + "," + e + "," + f); 
+
+// }
+
+
+
