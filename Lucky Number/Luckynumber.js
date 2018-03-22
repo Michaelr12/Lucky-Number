cf812b7abf3eb36b286e978b8a8b430b0c052e22
Michaelr12 <35987510+Michaelr12@users.noreply.github.com>
2018-03-21 19:22:44 -0700

    Update Luckynumber.js

--- Lucky Number/Luckynumber.js
+++ Lucky Number/Luckynumber.js
@@ -1,60 +1,18 @@
-
-
 function lotto(){
-	
-		var a = Math.floor(Math.random() * 100);
-	
-	var b = Math.floor(Math.random() * 100);
+	var numbers = [];
 
-	var c = Math.floor(Math.random() * 100);
-	
-	var d = Math.floor(Math.random() * 100);
+for(var i = 0; i < 6; i++) {
 
-	var e = Math.floor(Math.random() * 100);
+		numbers.push(Math.floor(Math.random(7) * 100));
+		
+	}
 
-	var f = Math.floor(Math.random() * 100);
+	return (_.uniq(numbers));
+		
+	}
 
+	var display = document.querySelector("#message");
 
-	if(a !== b && b !== c && c !== d && d !== e && e !== f) {
-	document.write(a + "," + b + "," + c + "," + d + "," + e + "," + f); 
-
-}
-
-// var number = document.querySelector("span");
-
-// button.addEventListener("click", function(){
-// 	reset();
-// })
-
-
-
-
-
-// 	for(var i = 0; i < 6; i++) {
-
-// 	var math(i) = Math.floor(Math.random() * 100)
-// 		if(a !== b && b !== c && c !== d && d !== e && e !== f) {
-// 	document.write(a + "," + b + "," + c + "," + d + "," + e + "," + f); 
-
-// }
-// 	}
-	
-
-
-// 	var b = Math.floor(Math.random() * 100);
-
-// 	var c = Math.floor(Math.random() * 100);
-	
-// 	var d = Math.floor(Math.random() * 100);
-
-// 	var e = Math.floor(Math.random() * 100);
-
-// 	var f = Math.floor(Math.random() * 100);
-
-// 	if(a !== b && b !== c && c !== d && d !== e && e !== f) {
-// 	document.write(a + "," + b + "," + c + "," + d + "," + e + "," + f); 
-
-// }
-
-
-
+button.addEventListener("click", function(){
+	display.textContent = lotto();
+})
